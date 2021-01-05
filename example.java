8bcb404a976410d7eed7ae9421d7efac6b5ce700
vidura-16 <71606311+vidura-16@users.noreply.github.com>
2021-01-05 23:03:18 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,18 @@
+public class example {
+    int m=20;
+      static int n=20;
+    public static void main(String[] args) {
+        new example().playwithval();
+        new example().playwithval();
+        new example().playwithval();
+    }
+void playwithval(){
+
+ int m=6;
+System.out.println("Sample n" + example.n);
+System.out.println("m " + m);
+n=n+3;
+
+}
+
+}
